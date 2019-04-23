0d5da74db69e5799e387e74958ddbe88d5fe1318
jolly1980 <thzoller@tsn.at>
2019-04-23 09:55:49 +0200

    entities

--- src/main/java/com/blog/config/CacheConfiguration.java
+++ src/main/java/com/blog/config/CacheConfiguration.java
@@ -38,6 +38,11 @@ public class CacheConfiguration {
             cm.createCache(com.blog.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.blog.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.blog.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.blog.domain.Blog.class.getName(), jcacheConfiguration);
+            cm.createCache(com.blog.domain.Entry.class.getName(), jcacheConfiguration);
+            cm.createCache(com.blog.domain.Entry.class.getName() + ".tags", jcacheConfiguration);
+            cm.createCache(com.blog.domain.Tag.class.getName(), jcacheConfiguration);
+            cm.createCache(com.blog.domain.Tag.class.getName() + ".entries", jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
